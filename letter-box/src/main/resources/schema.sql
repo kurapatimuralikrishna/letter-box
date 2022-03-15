98c33842540ac4be8543f689016155ae35572ce8
kurapatimuralikrishna <101548349+kurapatimuralikrishna@users.noreply.github.com>
2022-03-15 17:56:46 +0530

    Add files via upload

--- letter-box/src/main/resources/schema.sql
+++ letter-box/src/main/resources/schema.sql
@@ -1,7 +1,21 @@
-DROP TABLE IF EXISTS MOVIES;
-
-CREATE TABLE MOVIES(
-NAME VARCHAR(70) PRIMARY KEY,
-DIRECTOR VARCHAR(70),
-LANG VARCHAR(20),
-RATING FLOAT);
+CREATE TABLE user_profiles(
+		user_id INT PRIMARY KEY AUTO_INCREMENT,
+		username VARCHAR(70),
+		password VARCHAR(20),
+		phone_number VARCHAR(10),
+		city VARCHAR(20),
+		description VARCHAR(600)
+		);
+CREATE TABLE movies(
+		movie_name VARCHAR(70) PRIMARY KEY,
+		director VARCHAR(70),
+		lang VARCHAR(20),
+		rating FLOAT
+		);
+CREATE TABLE movie_reviews(
+		user_id INT ,
+		movie_name VARCHAR(70),
+		rating FLOAT,
+		review VARCHAR(600),
+        PRIMARY KEY(user_id,movie_name)
+		);
