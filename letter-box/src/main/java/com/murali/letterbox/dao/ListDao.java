1933373d18e020212a2d055f511bd3cb0c0dbb18
kurapatimuralikrishna <101548349+kurapatimuralikrishna@users.noreply.github.com>
2022-03-25 18:22:41 +0530

    Add files via upload

---
+++ letter-box/src/main/java/com/murali/letterbox/dao/ListDao.java
@@ -0,0 +1,11 @@
+package com.murali.letterbox.dao;
+
+import java.util.List;
+
+import com.murali.letterbox.model.MovieList;
+
+public interface ListDao {
+
+	List<MovieList> getAllLists();
+
+}
---
+++ letter-box/src/main/java/com/murali/letterbox/service/impl/ListServiceImpl.java
@@ -0,0 +1,23 @@
+package com.murali.letterbox.service.impl;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.murali.letterbox.dao.ListDao;
+import com.murali.letterbox.model.MovieList;
+import com.murali.letterbox.service.ListService;
+
+@Service
+public class ListServiceImpl implements ListService {
+	
+	@Autowired
+	ListDao dao;
+	
+	@Override
+	public List<MovieList> getAllLists() {
+		return dao.getAllLists();
+	}
+
+}
