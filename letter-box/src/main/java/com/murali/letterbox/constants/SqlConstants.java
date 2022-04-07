410f8f76bb0f50b12b5f540e384f2daf64bfc122
kurapatimuralikrishna <101548349+kurapatimuralikrishna@users.noreply.github.com>
2022-04-07 17:56:18 +0530

    Add files via upload

--- letter-box/src/main/java/com/murali/letterbox/constants/SqlConstants.java
+++ letter-box/src/main/java/com/murali/letterbox/constants/SqlConstants.java
@@ -5,14 +5,40 @@ public class SqlConstants {
 			"SELECT * FROM movies";
 
 	public static final String GET_ALL_LISTS = 
-			"SELECT * FROM movie_lists ml JOIN movies_inlist mil ON ml.list_id=mil.list_id;";
+			"SELECT * FROM movie_lists ml LEFT JOIN movies_inlist mil ON ml.list_id=mil.list_id ORDER BY ml.list_id;";
 	public static final String GET_LIST = 
 			"SELECT *\r\n"
 			+ "FROM movie_lists ml\r\n"
-			+ "JOIN movies_inlist mil\r\n"
+			+ "LEFT JOIN movies_inlist mil\r\n"
 			+ "	ON ml.list_id = mil.list_id\r\n"
 			+ "WHERE ml.user_id = ? AND ml.list_name = ?;";
 	public static final String CREATE_NEW_LIST = 
-			"INSERT INTO movie_lists\r\n"
+			"INSERT INTO movie_lists(user_id,list_name)\r\n"
 			+ "VALUES (?,?);";
+	public static final String GET_LIST_ID =
+			"SELECT list_id\r\n"
+			+ "FROM movie_lists\r\n"
+			+ "WHERE user_id=? AND list_name=?;";
+
+	public static final String ADD_MOVIE_TOLIST = 
+			"INSERT INTO movies_inlist(list_id,movie_name)\r\n"
+			+ "VALUES\r\n"
+			+ "	(?,?);";
+
+	public static final String UPDATE_LIST_NAME = 
+			"UPDATE movie_lists\r\n"
+			+ "SET list_name= ?\r\n"
+			+ "WHERE user_id=? AND list_name=?;";
+	
+	public static final String DELETE_MOVIES_OFLIST = 
+			"DELETE FROM movies_inlist\r\n"
+			+ "WHERE list_id=?;";
+
+	public static final String DELETE_MOVIE_FROMLIST = 
+			"DELETE FROM MOVIE_LISTS\r\n"
+			+ "WHERE list_id=? AND movie_name=?;";
+
+	public static final String DELETE_MOVIE_LIST = 
+			"DELETE FROM MOVIE_LISTS\r\n"
+			+ "WHERE list_id=?;";
 }
