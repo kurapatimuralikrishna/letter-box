c8bb644c3a40ea727359998eb7522d69a8eb26ce
kurapatimuralikrishna <kuraptimuralikrishna2405@gmail.com>
2022-04-20 17:16:18 +0530

    Welcome to main history

--- letter-box/src/main/java/com/murali/letterbox/LetterBoxApplication.java
+++ letter-box/src/main/java/com/murali/letterbox/LetterBoxApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LetterBoxApplication {
 
-	
+	//Welcome
 	public static void main(String[] args) {
 		SpringApplication.run(LetterBoxApplication.class, args);
 	}
